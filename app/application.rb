f50e263b73dbc034c8dfa5405d187b866a428b71
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-30 21:59:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,7 @@ class Application
       if req.path.match(/items\/./)
         item_name = req.path.split("/items").last 
         item = @@items.detect do |i| 
-          
+          i.name = item.name 
+        end 
       end
   end 
