270c952a3bf018fb5fe79e7f83cee9937d41a262
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-30 21:58:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,6 @@ class Application
     req = Rack::Request.new(env)
     
       if req.path.match(/items\/./)
-      @@items.each do |item|
-        resp.write "#{item}\n"
+        
       end
   end 
