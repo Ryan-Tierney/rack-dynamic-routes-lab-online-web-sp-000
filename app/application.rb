d494c3b974998c32bdf8437c0e30d42dda657ebb
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-30 22:18:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,25 +1,25 @@
 class Application
-  
+
   @@items = []
-  
-   def call(env)
+
+  def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
-    
-      if req.path.match(/items\/.+/)
-        item_name = req.path.split("/items/").last 
-        item = @@items.detect { |i| i.name ==  item.name}
-        if item 
-          resp.write item.price 
-        else 
-          resp.write "Item not found"
-          resp.status = 400
-        end 
-      else 
-        resp.write "Route not found"
-        resp.status = 404
+
+    if req.path.match(/items\/.+/)
+      item_name = req.path.split("/items/").last
+      item = @@items.detect { |i| i.name == item_name }
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
       end
-      
-    resp.finish 
-  end 
-end   
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
